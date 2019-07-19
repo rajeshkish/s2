577773408c941cb416dc990b398c99bbfb0838ca
rajeshkish <52270436+rajeshkish@users.noreply.github.com>
2019-07-19 16:47:34 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,5 +1,7 @@
-ch=input()
-if ch in ['a','e','i','o','u']:
-print('vowel')
+p=str(input())
+if (p=='a' or p=='A' or p=='e' or p=='E' or p=='i' or p=='I' or p=='o' or p=='O' or p=='u' or p=='U'):
+    print('Vowel')
+elif (p>='a' and p<='z') or (p>='A' and p<='Z'):
+    print('Consonant')
 else:
-print('consonant')
+    print('invalid')
